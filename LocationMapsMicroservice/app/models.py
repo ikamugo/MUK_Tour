cf6a639f4aaee5564b34c7d4c27ec419ce1076c5
BukenyaLukman <bukenyalukman@gmail.com>
2021-02-13 15:19:38 +0300

    The First test

--- LocationMapsMicroservice/app/models.py
+++ LocationMapsMicroservice/app/models.py
@@ -4,4 +4,9 @@ Definition of models.
 
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+	name = models.CharField(max_length=200, null=True, blank=True)
+	position = models.CharField(max_length=200, null=True, blank=True)
+
+	def __str__(self):
+		return self.name + " " + self.position
--- LocationMapsMicroservice/app/views.py
+++ LocationMapsMicroservice/app/views.py
@@ -4,7 +4,11 @@ Definition of views.
 
 from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest,JsonResponse, HttpResponse
+
+import requests
+
+from . models import Place
 
 def home(request):
     """Renders the home page."""
